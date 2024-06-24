91d0319dd8dcdd82332ecb349804adc453db45c1
leozhang <it@tparts.com>
2024-06-24 22:11:05 +0800

    feat: update Dockerfile and support https_proxy
    Signed-off-by: leozhang <it@tparts.com>

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,13 @@ WORKDIR /app
 ENV GOPROXY=https://goproxy.cn,direct
 COPY go.mod go.sum ./
 COPY . .
-RUN go mod download
-RUN GOOS=linux GOARCH=amd64 go build -o ytb-downloader .
+RUN go mod download && GOOS=linux GOARCH=amd64 go build -o ytb-downloader .
 
 FROM debian:stable-slim
 WORKDIR /app
 COPY --from=builder /app/ytb-downloader .
 COPY --from=builder /app/web/dist ./web/dist
-EXPOSE 7777
-CMD ["./ytb-downloader"]
+ENV HTTP_ADDR=":7777"
 
+EXPOSE 7777
+CMD ["./ytb-downloader","-http-addr=${HTTP_ADDR}"]
--- downloader.go
+++ downloader.go
@@ -1,7 +1,33 @@
 package main
 
-import ytdl "github.com/kkdai/youtube/v2/downloader"
+import (
+	"crypto/tls"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/kkdai/youtube/v2"
+	ytdl "github.com/kkdai/youtube/v2/downloader"
+)
 
 func initDownloader() {
-	ytbDownloader = &ytdl.Downloader{}
+	httpCli := &http.Client{}
+
+	proxyAddr, err := url.Parse(os.Getenv("HTTPS_PROXY"))
+	if err == nil {
+		httpCli = &http.Client{
+			Transport: &http.Transport{
+				Proxy: http.ProxyURL(proxyAddr),
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: true,
+				},
+			},
+		}
+	}
+
+	ytbDownloader = &ytdl.Downloader{
+		Client: youtube.Client{
+			HTTPClient: httpCli,
+		},
+	}
 }
